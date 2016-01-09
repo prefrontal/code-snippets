9cbc00ea57b1219055981d508e20817f19252518
Craig Bennett <prefrontal@gmail.com>
2016-01-08 19:39:16 -0800

    Block syntax

---
+++ C/Blocks.c
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------------------------
+
+// As a local variable:
+
+returnType (^blockName)(parameterTypes) = ^returnType(parameters) {...};
+
+// As a property:
+
+@property (nonatomic, copy) returnType (^blockName)(parameterTypes);
+
+// As a method parameter:
+
+- (void)someMethodThatTakesABlock:(returnType (^)(parameterTypes))blockName;
+
+// As an argument to a method call:
+
+[someObject someMethodThatTakesABlock:^returnType (parameters) {...}];
+
+// As a typedef:
+
+typedef returnType (^TypeName)(parameterTypes);
+TypeName blockName = ^returnType(parameters) {...};
+
+// From: http://fuckingblocksyntax.com
+
+------------------------------------------------------------------------------------------
