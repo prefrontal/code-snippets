bf868cc900c69e775e8edfc0a20b1439d761d729
Craig Bennett <prefrontal@gmail.com>
2016-02-07 21:30:45 -0800

    More on Swift Protocols

--- Swift/Protocols.swift
+++ Swift/Protocols.swift
@@ -17,3 +17,30 @@
 	}
 	
 // Classes, enumerations, and structs can all adopt protocols.
+
+	// Protocol definition
+	protocol TabularDataSource
+	{
+		var numberOfRows:Int {get}
+		var numberOfColumns:Int {get}
+	}
+	
+	struct Department: TabularDataSource
+	{
+		var numberOfRows:Int
+		{
+			return people.count
+		}
+
+		var numberOfColumns:Int
+		{
+			return 2
+		}
+	}
+	
+// You can even conform to multiple protocols using protocol composition
+
+	func printTable (dataSource: protocol<TabularDataSource, CustomStringConvertible>)
+	{
+		print ("Table: \(dataSource.description)")
+	}
