aecdb9f124c73bd60dea365be7e5cfbbf660c4c4
Craig Bennett <prefrontal@gmail.com>
2016-03-03 23:20:22 -0800

    More Swift closures

--- Swift/Closures.swift
+++ Swift/Closures.swift
@@ -42,3 +42,36 @@ var alsoDouble = double
 alsoDouble(3) // 6
 
 // From: https://www.weheartswift.com/closures/
+
+// -----------------------------------------------------------------------------
+
+// Passing functions and closures around
+
+func testAdd (a:Int, b:Int) -> Int
+{
+    return a+b
+}
+
+var myFunc = testAdd
+
+func testClose (a a:Int, b:Int, c:(Int,Int)->Int) -> Int
+{
+    return c(a,b)
+}
+
+let first = testClose (a:4, b:5, c:myFunc)
+print (first)
+
+let second = testClose (a:3, b:4) {x,y in return x*y}
+print (second)
+
+let third = testClose (a:5, b:3) {return $0*$1}
+print (third)
+
+var fourthClosure:(Int,Int) -> (Int) = {x,y in return x*y}
+let fourth = testClose (a:5, b:4, c:fourthClosure)
+print (fourth)
+
+var fifthClosure:(Int,Int) -> (Int) = {return $0*$1}
+let fifth = testClose (a:6, b:4, c:fifthClosure)
+print (fifth)
