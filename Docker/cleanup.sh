bc214ece62e726ec1592d43dd936362a3719cb23
Craig Bennett <prefrontal@gmail.com>
2016-08-16 22:41:17 -0700

    Move devops stuff out

--- Docker/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clear all the cruft that has been accumulating in Docker
-
-# Delete all containers
-docker rm $(docker ps -a -q)
-
-# Delete all images
-docker rmi $(docker images -q)
