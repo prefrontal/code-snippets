e9ce0a4d107666c3b4af5bb57e7ee1cb15dcbb28
Craig Bennett <prefrontal@gmail.com>
2016-09-18 09:19:53 -0700

    Dateutil timezone information

---
+++ Python-Modules/Dateutil/Timezones.py
@@ -0,0 +1,9 @@
+# -- Dateutil Timezones ------------------------------------------------------------------
+
+# Get timezone definitions
+
+	from dateutil.tz import *
+	
+	timezone = gettz("America/Sao Paulo")
+
+# ----------------------------------------------------------------------------------------
