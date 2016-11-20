bf2de206a9178f3333832c47ed9caa8a4b34aeb1
Craig Bennett <prefrontal@users.noreply.github.com>
2016-11-19 16:03:07 -0800

    Numpy array boolean logic

---
+++ Python-Modules/Numpy/array_boolean_logic.py
@@ -0,0 +1,6 @@
+# ---- Numpy Boolean Logic -----------------------------------------------------------------------
+
+logical_and(x1, x2[, out])	    # Compute the truth value of x1 AND x2 element-wise.
+logical_or(x1, x2[, out])	      # Compute the truth value of x1 OR x2 element-wise.
+logical_not(x[, out])	          # Compute the truth value of NOT x element-wise.
+logical_xor(x1, x2[, out])	    # Compute the truth value of x1 XOR x2, element-wise.
