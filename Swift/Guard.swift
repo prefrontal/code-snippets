0927564165c09e0308457af41529a5c77bc79d75
Craig Bennett <prefrontal@users.noreply.github.com>
2017-01-03 14:41:40 -0800

    Alternative guard syntax

--- Swift/Guard.swift
+++ Swift/Guard.swift
@@ -5,3 +5,15 @@ guard let user = username else
     // username is nil, prompt for login.
     return
 }
+
+
+
+func checkAuthenticated(authenticated: Bool) -> Bool {
+    guard authenticated else {
+        // Do something in response to failure
+        
+        return false
+    }
+    
+    return true
+}
